00318eca60d542597d7b6c56e45664df91e9ae78
zirkelc <chris.zirkel@gmail.com>
2023-05-26 14:13:13 +0200

    add use data dir command line flag

--- resources/macos/start.sh
+++ resources/macos/start.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222&
+
+USER_DATA_DIR="$HOME/Library/Application Support/Google/Chrome Debugger"
+
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --user-data-dir="$USER_DATA_DIR"&
